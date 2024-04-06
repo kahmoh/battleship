5d609a54bf6658b90f69a46b4d821ab16fd1d238
kahmoh <joshmcfadden17@gmail.com>
2024-04-06 15:40:05 +0100

    setup initial project structure -install dependencies -write gitignore -create initial file structure

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: './dist',
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+            template: './src/index.html',
+            inject: 'body',
+            filename: 'index.html',
+        })
+    ]
+};
