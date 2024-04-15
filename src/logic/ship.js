ffbd418b9663ef29a43d267c1a4f62e706bd80b3
kahmoh <joshmcfadden17@gmail.com>
2024-04-15 16:39:26 +0100

    change path for npm watch script to src folder

--- src/logic/ship.js
+++ src/logic/ship.js
@@ -9,3 +9,5 @@ const Ship = {
 
     },
 }
+
+export {Ship}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,5 @@
+import {Ship} from '../logic/ship.js';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(Ship.hit()).toBe('hit');
+});
