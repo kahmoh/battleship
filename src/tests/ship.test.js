8323b2ea229edc055dda7907a6d10d955669d718
kahmoh <joshmcfadden17@gmail.com>
2024-04-16 12:50:04 +0100

    write tests for hit function with all possible numbers of hits based on ship sizes

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -4,3 +4,33 @@ test('returns 1', () => {
     const testShip = Ship();
     expect(testShip.hit()).toBe(1);
 });
+
+test('returns 2', () => {
+    const testShip = Ship();
+    testShip.hit();
+    expect(testShip.hit()).toBe(2);
+})
+
+test('returns 3', () => {
+    const testShip = Ship();
+    testShip.hit()
+    testShip.hit();
+    expect(testShip.hit()).toBe(3);
+})
+
+test('returns 4', () => {
+    const testShip = Ship();
+    testShip.hit()
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.hit()).toBe(4);
+})
+
+test('returns 5', () => {
+    const testShip = Ship();
+    testShip.hit()
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.hit()).toBe(5);
+})
