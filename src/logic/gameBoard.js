fe898845c3f02645959ef1884d70adaf4695eb00
kahmoh <joshmcfadden17@gmail.com>
2024-04-22 17:19:49 +0100

    add functionality to render game boards

--- src/logic/gameBoard.js
+++ src/logic/gameBoard.js
@@ -4,9 +4,22 @@ const GameBoard = function () {
     const createBoard = function () {
         const container = document.createElement("div");
         container.classList.add('game-board-backdrop');
+        for(let i = 0; i < 10; i++) {
+            const gameBoardRow = Object.assign(document.createElement('div'),{
+                id: i,
+                className: 'game-board-row',
+            })
+            container.appendChild(gameBoardRow)
+            for (let j = 0; j < 10; j++) {
+                const gameBoardTile = Object.assign(document.createElement('div'), {
+                    id: j,
+                    className: 'game-board-tile'
+                })
+                gameBoardRow.append(gameBoardTile)
+            }
+        }
         document.body.appendChild(container);
     }
-
     return {createBoard}
 }
 
