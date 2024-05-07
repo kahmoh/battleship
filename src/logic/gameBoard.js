cf56a6e16431b17e5afcb71a5962ee7bc60bbe22
kahmoh <joshmcfadden17@gmail.com>
2024-05-07 17:16:37 +0100

    refactor gameBoard factory -refactor createBoard function to remove adding of eventListeners when gameBoard is created -refactor getCoordinate to return an object with x and y values

--- src/logic/gameBoard.js
+++ src/logic/gameBoard.js
@@ -1,9 +1,19 @@
-import '../styles/gameBoard.style.css'
+// import '../styles/gameBoard.style.css'
 
 const GameBoard = function () {
+    const gameBoardArray = []
+
+    for (let i = 0; i < 10 ;i++){
+        let row = []
+        for (let j = 0; j < 10; j++){
+            row.push(0)
+        }
+        gameBoardArray.push(row)
+    }
+
     const createBoard = function () {
         const container = document.createElement("div");
-        container.classList.add('game-board-backdrop');
+        // container.classList.add('game-board-backdrop');
         for(let i = 0; i < 10; i++) {
             const gameBoardRow = Object.assign(document.createElement('div'),{
                 id: i,
@@ -15,21 +25,21 @@ const GameBoard = function () {
                     id: `${i}${j}`,
                     className: 'game-board-tile',
                 })
-                gameBoardTile.addEventListener('click', (event) => {
-                    const x = event.target.parentNode.id
-                    const y = event.target.id
-                    event.target.classList.add('game-board-tile-with-ship')
-                    getCoordinate({x,y})
-                })
                 gameBoardRow.append(gameBoardTile)
             }
         }
         document.body.appendChild(container);
     }
-    const getCoordinate = function (tile) {
-        console.log ({x: tile.x, y: tile.y})
+    const getCoordinates = function (tile) {
+        const unsplitCoordinates = String(tile)
+        const x = unsplitCoordinates.slice(0, unsplitCoordinates.length / 2)
+        const y = unsplitCoordinates.slice(unsplitCoordinates.length / 2, unsplitCoordinates.length)
+        return {
+            x: Number(x),
+            y: Number(y)
+        }
     }
-    return {createBoard}
+    return {createBoard,gameBoardArray,getCoordinates}
 }
 
 export {GameBoard}
