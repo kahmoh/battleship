c2895b5e48554339e6e0c302b9824e0276e27f1d
kahmoh <joshmcfadden17@gmail.com>
2024-05-13 20:47:33 +0100

    remove unused game board array creation

--- src/logic/gameBoard.js
+++ src/logic/gameBoard.js
@@ -1,15 +1,6 @@
 import '../styles/gameBoard.style.css'
 
 const GameBoard = function () {
-    const gameBoardArray = []
-
-    for (let i = 0; i < 10 ;i++){
-        let row = []
-        for (let j = 0; j < 10; j++){
-            row.push(0)
-        }
-        gameBoardArray.push(row)
-    }
 
     const createBoard = function () {
         const container = document.createElement("div");
