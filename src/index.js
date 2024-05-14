7ea5352d7e6c448d4fa7c6c7ac8df160700601f8
kahmoh <joshmcfadden17@gmail.com>
2024-05-14 16:57:37 +0100

    remove old code used for testing

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import {GameBoard} from "./logic/gameBoard.js";
-import {Ship} from "./logic/ship.js";
-import {Player} from "./logic/player.js";
-
-const thisBoard = GameBoard()
-
-thisBoard.createBoard()
