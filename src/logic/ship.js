12270f95efd0492be27e94dbb5d71eed1f71d1f8
kahmoh <joshmcfadden17@gmail.com>
2024-05-18 22:52:49 +0100

    refactor ship factory to include render ship function which checks for a valid array and a gameboard

--- src/logic/ship.js
+++ src/logic/ship.js
@@ -1,12 +1,20 @@
 import '../styles/gameBoard.style.css'
 
-const Ship = (coordinates) => {
+const Ship = (coordinates,board) => {
     let hitCounter = 0
 
-    for (let i = 0; i < coordinates.length; i++){
-        const tile = document.getElementById(coordinates[i])
-        tile.classList.add('game-board-tile-with-ship')
+    const renderShip = function () {
+        if (board !== undefined) {
+            if (coordinates.constructor === Array && coordinates.length) {
+                for (let i = 0; i < coordinates.length; i++){
+                    const tile = document.getElementById(coordinates[i])
+                    tile.classList.add('game-board-tile-with-ship')
+                }
+            }
+        }
     }
+
+    renderShip()
 
     const hit = function () {
         return hitCounter += 1;
