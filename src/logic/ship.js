97cbe67b17591e8e38cd733b181d27ed7dc6844a
kahmoh <joshmcfadden17@gmail.com>
2024-06-18 21:49:06 +0100

    remove leftover helper functions for placeShip

--- src/logic/ship.js
+++ src/logic/ship.js
@@ -3,17 +3,6 @@ import '../styles/gameBoard.style.css';
 const Ship = (board, shipID, length) => {
   let hitCounter = 0;
 
-  function checkShipElementExists() {
-    if (document.getElementsByClassName(shipID)[0] !== null) {
-      return document.getElementsByClassName(shipID)[0];
-    }
-    return 'ship element doesnt exist';
-  }
-
-  function shipPlacementIsValid() {
-
-  }
-
   function hit() {
     hitCounter += 1;
     return hitCounter;
