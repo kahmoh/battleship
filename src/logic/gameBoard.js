99709917a61153fa6e7b8e89a75b17b0d7a31e18
kahmoh <joshmcfadden17@gmail.com>
2024-06-20 20:44:42 +0100

    refactor ship placing functionality to allow for ship squares and surrounding squares to be occupied

--- src/logic/gameBoard.js
+++ src/logic/gameBoard.js
@@ -22,39 +22,64 @@ function GameBoard() {
   }
 
   function coordinateIsValid(coordinate) {
-    try {
-      if (coordinate > 0 && coordinate < 100) {
-        return true;
-      }
-    } catch (error) {
-      console.log(coordinate);
-      return false;
+    if (document.getElementById(coordinate)){
+      return true
+    }else {
+      return `${coordinate} is not a tile`
     }
   }
 
-  function shipTiles(startingCoordinate, ship) {
+  function shipTiles(startingCoordinate, ship, shipOrientation) {
     const coordinates = [];
-    for (let i = 1; i < ship.length; i += 1) {
-      const coordinate = (startingCoordinate + i);
-      if (coordinateIsValid(coordinate)) {
-        coordinates.push(coordinate);
+    for (let i = 0; i< 2; i++ ){
+      coordinates.push(startingCoordinate)
+      for (let j = 1; j < ship; j += 1) {
+        const coordinate = (Number(startingCoordinate) + j);
+        if (coordinateIsValid(coordinate) === true) {
+          coordinates.push(String(coordinate));
+        }else{
+          return `invalid placement`
+        }
       }
+      let startingCoordinateArray = startingCoordinate.split('')
+      if (i === 0) {
+        startingCoordinateArray[0] = Number(startingCoordinateArray[0]) + 1
+      }else if (i === 1) {
+        startingCoordinateArray[0] = Number(startingCoordinateArray[0]) - 2
+      }
+      startingCoordinateArray[0] = startingCoordinateArray[0].toString()
+      startingCoordinate = startingCoordinateArray.join('')
     }
+    console.log(coordinates)
+    return coordinates
   }
 
-  function tileIsEmpty() {
-
+  function tileIsEmpty(tile) {
+    if (document.getElementById(tile).className !== 'game-board-tile-with-ship'){
+      return true
+    }else{
+      return `${tile} is busy`
+    }
   }
 
   function placeShip(startingCoordinate, shipID) {
-    const shipElement = document.getElementById(shipID);
+    const shipElement = document.getElementsByClassName(shipID)[0];
     const startingTile = document.getElementById(startingCoordinate);
+    const shipTilesArray = shipTiles(startingCoordinate,4)
+    for (let i = 0; i < shipTilesArray.length; i += 1) {
+      const targetTile = document.getElementById(shipTilesArray[i])
+      targetTile.classList.add('game-board-tile-with-ship')
+    }
     shipElement.style.position = 'fixed';
     shipElement.style.top = `${startingTile.getBoundingClientRect().y - 10}px`;
     shipElement.style.left = `${startingTile.getBoundingClientRect().x - 10}px`;
   }
 
-  return { createBoard, placeShip, coordinateIsValid };
+  function defaultShipPlacement() {
+
+  }
+
+  return { createBoard, placeShip, coordinateIsValid, shipTiles };
 }
 
 export { GameBoard };
