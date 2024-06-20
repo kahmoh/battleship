73650d0f9336e40b58e85a94415af0e8b256ff92
kahmoh <joshmcfadden17@gmail.com>
2024-06-20 20:42:55 +0100

    include file extension for player.js import & begin writing logic for gameplay

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,9 +1,13 @@
 import '../styles/game.style.css';
+import {Player} from "./player.js";
 
 function Game() {
 
   function playGame() {
-
+    const player1 = Player('Player 1')
+    player1.playerBoard.createBoard()
+    player1.renderShips()
+    player1.randomShipPositions()
   }
 
   return { playGame };
