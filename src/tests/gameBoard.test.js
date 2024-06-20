860140eb84dea75bd6909bd544780a274581949b
kahmoh <joshmcfadden17@gmail.com>
2024-06-20 14:36:03 +0100

    remove unused tests from gameBoard.test.js

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,21 +1,10 @@
 import {GameBoard} from "../logic/gameBoard.js";
+import {Game} from "../logic/game.js";
 
-test('getCoordinates(14) returns {x:1, y:4}', () => {
-    const testBoard = GameBoard()
-    expect(testBoard.getCoordinates(14)).toStrictEqual({x:1, y:4})
-})
-
-test('getCoordinates(99) returns {x:9, y:9}', () => {
-    const testBoard = GameBoard()
-    expect(testBoard.getCoordinates(99)).toStrictEqual({x:9, y:9})
-})
-
-test('getCoordinates(26) returns {x:2, y:6}', () => {
-    const testBoard = GameBoard()
-    expect(testBoard.getCoordinates(26)).toStrictEqual({x:2, y:6})
-})
-
-test('getCoordinates(71) returns {x:7, y:1}', () => {
-    const testBoard = GameBoard()
-    expect(testBoard.getCoordinates(71)).toStrictEqual({x:7, y:1})
+test('shipTiles() returns correct tiles', () => {
+    const testGame = Game()
+    const testGameBoard = GameBoard()
+    testGame.renderShips()
+    testGameBoard.createBoard()
+    expect(testGameBoard.shipTiles('11','five')).toStrictEqual(["11","12",'13',"14","15","01","02","03","04","05","21","22","23","24","25"])
 })
