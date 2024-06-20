7ffa1b1ebbe0a5a70ff09f5f4794976b3980ea3c
kahmoh <joshmcfadden17@gmail.com>
2024-06-20 15:01:55 +0100

    move ship rendering functions from game.js to player.js

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,31 +1,6 @@
 import '../styles/game.style.css';
 
 function Game() {
-  function shipElement(shipSize) {
-    return Object.assign(document.createElement('div'), {
-      className: String(shipSize),
-    });
-  }
-
-  const convertNumbersToWords = {
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-  };
-
-  function renderShips() {
-    for (let i = 2; i < 6; i += 1) {
-      const shipClassName = convertNumbersToWords[i];
-      if (i !== 2) {
-        document.body.append(shipElement(shipClassName));
-      } else {
-        document.body.append(shipElement(shipClassName));
-        document.body.append(shipElement(shipClassName));
-      }
-    }
-  }
 
   function playGame() {
 
--- src/logic/player.js
+++ src/logic/player.js
@@ -1,7 +1,37 @@
 import { GameBoard } from './gameBoard';
+import {Ship} from "./ship.js";
 
 const Player = (name) => {
   const playerBoard = GameBoard();
+
+  const ships = []
+
+  function shipElement(shipSize) {
+    return Object.assign(document.createElement('div'), {
+      className: String(shipSize),
+    });
+  }
+
+  const convertNumbersToWords = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+  };
+
+  function renderShips() {
+    for (let i = 2; i < 6; i += 1) {
+      const shipClassName = convertNumbersToWords[i];
+      if (i !== 2) {
+        document.body.append(shipElement(shipClassName));
+      } else {
+        document.body.append(shipElement(shipClassName));
+        document.body.append(shipElement(shipClassName));
+      }
+    }
+  }
+
 
   return { name, playerBoard };
 };
