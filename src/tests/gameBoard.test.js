549e9d7f21741a75e75fd3c3c3ea2b4557183215
kahmoh <joshmcfadden17@gmail.com>
2024-06-21 16:12:03 +0100

    write tests for coordinateRow function

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,2 +1,11 @@
 import {GameBoard} from "../logic/gameBoard.js";
 
+test('returns 21', () => {
+    const testGameBoard = GameBoard()
+    expect(testGameBoard.coordinateRow("11",1)).toBe("21")
+})
+
+test('returns 01', () => {
+    const testGameBoard = GameBoard()
+    expect(testGameBoard.coordinateRow("11",-1)).toBe("01")
+})
