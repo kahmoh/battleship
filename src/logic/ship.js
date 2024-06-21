7a0fbab321f9ba25261eaf8f326ce892cc5d0da6
kahmoh <joshmcfadden17@gmail.com>
2024-06-21 16:07:23 +0100

    refactor ship parameters and ship isSunk tests to allow isSunk tests to pass

--- src/logic/ship.js
+++ src/logic/ship.js
@@ -1,6 +1,6 @@
 import '../styles/gameBoard.style.css';
 
-const Ship = (board, shipID, length) => {
+const Ship = (length, board, shipID) => {
   let hitCounter = 0;
 
   function hit() {
@@ -8,6 +8,7 @@ const Ship = (board, shipID, length) => {
     return hitCounter;
   }
   function isSunk() {
+    console.log(length)
     return hitCounter >= length;
   }
   return {
