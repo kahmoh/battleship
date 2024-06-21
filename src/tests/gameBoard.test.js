53ba42db9a13e9374d87be01a7c6bee4bb2a0d49
kahmoh <joshmcfadden17@gmail.com>
2024-06-21 15:52:26 +0100

    remove unused tests for shipTiles function to be refactored

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,10 +1,2 @@
 import {GameBoard} from "../logic/gameBoard.js";
-import {Game} from "../logic/game.js";
 
-test('shipTiles() returns correct tiles', () => {
-    const testGame = Game()
-    const testGameBoard = GameBoard()
-    testGame.renderShips()
-    testGameBoard.createBoard()
-    expect(testGameBoard.shipTiles('11','five')).toStrictEqual(["11","12",'13',"14","15","01","02","03","04","05","21","22","23","24","25"])
-})
